f4baae34fd1f3e5f192c0a0c1c6a1452140a8a93
Gloria <Gloria@Gloria-PC>
2023-01-02 00:31:16 +1300

    km to miles

---
+++ shortcut/src/shortcut/shortCut.java
@@ -0,0 +1,18 @@
+package shortcut;
+import java.util.Scanner;
+
+public class shortCut 
+{
+public static void main(String[] args) 
+{
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Please enter the kilometer value");
+	double km = sc.nextDouble();
+	double miles = km*0.6;
+	System.out.println(miles+ "miles");
+	
+	
+}
+
+	}
+
